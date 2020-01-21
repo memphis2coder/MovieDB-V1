ae56ff298dc95775cd6404d7ae386e70f783036b
Antonio <Memphis2Coder@gmail.com>
2020-01-20 21:14:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import axios from "axios";
+import "./App.css";
+// components
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      searchTerm: ""
+    };
+    this.apiKey = process.env.REACT_APP_API_KEY;
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <div className="container">
+          <Hero />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+// const API_URL =
+//   "https://api.themoviedb.org/3/movie/76341?api_key=process.env.REACT_APP_API_KEY";
+
+// state = {
+//   componentDidMount() {
+//     fetch(API_URL)
+//       .then(res => res.json())
+//       .then();
+//   }
+// };
+const Navigation = props => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            MDB Search
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  MOVIES <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  TV SHOWS
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  PEOPLE
+                </a>
+              </li>
+            </ul>
+            <form
+              className="form-inline my-2 my-lg-0"
+              onSubmit={props.handleSubmit}
+            >
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                name="userSearch"
+              />
+              <button
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
