41a677d5bd7d3bdc89adca500dca829bc6b2182a
Antonio <Memphis2Coder@gmail.com>
2020-01-24 20:39:50 -0500

    added bs4 cards

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { API_KEY, API_URL, API_IMG } from "../../Config";
 
 import Hero from "../Hero/Hero";
-import LatestMovies from "./LatestMovies/LatestMovies";
+import PopularMovies from "./PopularMovies/PopularMovies";
 
 function LandingPage() {
   const [movies, setMovies] = useState([]);
@@ -20,23 +20,31 @@ function LandingPage() {
     <div>
       <div className="app">
         {/* Hero Image */}
-        {movies[0] && (
-          <Hero
-            image={`${API_IMG}w1280${movies[0].backdrop_path &&
-              movies[0].backdrop_path}`}
-            title={movies[0].original_title}
-            text={movies[0].overview}
-          />
-        )}
+        <section id="hero">
+          <div>
+            {movies[0] && (
+              <Hero
+                image={`${API_IMG}w1280${movies[0].backdrop_path &&
+                  movies[0].backdrop_path}`}
+                title={movies[0].original_title}
+                text={movies[0].overview}
+              />
+            )}
+          </div>
+        </section>
+
         {/* LatestMovies */}
         <section id="latestMovies">
           <div>
             {movies &&
               movies.map((movie, index) => (
-                <LatestMovies
+                <PopularMovies
+                  key={index}
                   image={
-                    movie.poster_path && `${API_IMG}w500${movie.poster_path}`
+                    movie.poster_path && `${API_IMG}w200${movie.poster_path}`
                   }
+                  title={movie.title}
+                  movieId={movie.id}
                 />
               ))}
             ;
---
+++ src/components/LandingPage/PopularMovies/PopularMovies.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./PopularMovies.css";
+
+const PopularMovies = props => {
+  return (
+    <div className="popularMovies">
+      <h1>Popular Movies</h1>
+      <hr />
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-6 col-md-3">
+            one of four columns
+            <div className="card text-dark shadow custom-card border-dark">
+              <img src={props.image} className="card-img-top" alt="img" />
+              <div className="card-body ">
+                <h5 className="card-title">{props.title}</h5>
+                <a href="#" className="stretched-link"></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-md-3">
+            two of four columns
+            <div className="card text-dark shadow custom-card border-dark">
+              <img src={props.image} className="card-img-top" alt="img" />
+              <div className="card-body ">
+                <h5 className="card-title">{props.title}</h5>
+                <a href="#" className="stretched-link"></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-md-3">three of four columns</div>
+          <div className="col-6 col-md-3">four of four columns</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularMovies;
+
+/* <div className="card text-dark">
+              <img src="" className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="stretched-link"></a>
+              </div>
+            </div> */
